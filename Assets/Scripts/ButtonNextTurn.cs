259144b6f67d0c359e0454f671c01394914400cb
Denis Melnikov <demelnikov@gmail.com>
2025-03-30 22:23:37 +0300

    Button nextTurn is Switching pause

--- Assets/Scripts/ButtonNextTurn.cs
+++ Assets/Scripts/ButtonNextTurn.cs
@@ -4,8 +4,10 @@ using UnityEngine.UI;
 
 public class ButtonNextTurn : MonoBehaviour
 {
+    [SerializeField] private bool IsPause = true;
     public delegate void OnNextTurn();
-    public static event  OnNextTurn onNextTurn;
+    public static event OnNextTurn onNextTurn;
+    
 
     //onNextTurn 
 
@@ -20,6 +22,25 @@ public class ButtonNextTurn : MonoBehaviour
     private void test1()
     {
         Debug.LogFormat("pressed - test");
+    }
+
+    public void SwitchPause()
+    {
+        if (IsPause) 
+        { 
+            IsPause = false;
+            this.gameObject.GetComponent<Image>().color = Color.green;
+        } 
+        else 
+        { 
+            IsPause = true;
+            this.gameObject.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public bool GetIsPause()
+    {
+        return IsPause;
     }
 
 
